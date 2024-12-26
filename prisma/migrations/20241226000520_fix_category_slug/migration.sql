e967e2bfadfcb4d0fbc1b3a934b498bdbd91d9a2
Pedro dos Santos <pedroblues281@gmail.com>
2024-12-25 22:39:45 -0300

    feat: add seed script to prisma and imageUrls field to Product table

---
+++ prisma/migrations/20241226000520_fix_category_slug/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ImageUrl` on the `Category` table. All the data in the column will be lost.
+  - You are about to drop the column `Slug` on the `Category` table. All the data in the column will be lost.
+  - Added the required column `imageUrl` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `slug` to the `Category` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" DROP COLUMN "ImageUrl",
+DROP COLUMN "Slug",
+ADD COLUMN     "imageUrl" TEXT NOT NULL,
+ADD COLUMN     "slug" TEXT NOT NULL;
