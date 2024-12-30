0a017abd96dbae25bc72098161140aafb2eb61da
Pedro dos Santos <pedroblues281@gmail.com>
2024-12-30 19:46:28 -0300

    fix: ensure correct product total priceis calculated

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -11,10 +11,10 @@ export const computeProductTotalPrice = (product: Product): ProductWithTotalPric
         }
     }
 
-    const totalPrice = Number(product.basePrice) + (product.discountPercent/100)
+    const totalDiscount = Number(product.basePrice) + (product.discountPercent/100)
 
     return {
        ...product,
-        totalPrice,
+        totalPrice: Number(product.basePrice) - totalDiscount,
     }
 }
