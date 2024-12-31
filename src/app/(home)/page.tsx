d843bfdac2ccbf1bef2b46e94e28cde1723d5c86
Pedro dos Santos <pedroblues281@gmail.com>
2024-12-30 23:21:26 -0300

    feat: add banner second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase"> Ofertas </p>
         <ProductHorizontalList products={deals} />
       </div>
+      
+      <Image
+        src="/banner-home-02.png"
+        width={0}
+        height={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses"
+      />
     </div>
   );
 }
