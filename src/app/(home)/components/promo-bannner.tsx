809111dbece9e3a5dca7664516310ec67b4d2ecc
Pedro dos Santos <pedroblues281@gmail.com>
2024-12-30 23:35:28 -0300

    feat: add PromoBanner and SectionTitle components

---
+++ src/app/(home)/components/promo-bannner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { ImageProps } from "next/image";
+
+const PromoBanner = ({alt,...props}: ImageProps) => {
+  return (
+    <Image
+      width={0}
+      height={0}
+      className="h-auto w-full px-5"
+      sizes="100vw"
+      alt={alt}
+      {...props}
+    />
+  );
+};
+
+export default PromoBanner;
