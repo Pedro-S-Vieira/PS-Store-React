4545477c75a35deab7629f635903b5822ed77556
Pedro dos Santos <pedroblues281@gmail.com>
2025-02-27 21:32:57 -0300

    feat: add CatalogPage

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,26 @@
+import { Badge } from "@/components/ui/badge";
+import { LayoutGrid } from "lucide-react";
+import { prismaClient } from "@/app/lib/prisma";
+import CatagoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+  return (
+    <div className="p-5">
+      <Badge
+        variant="heading"
+      >
+        <LayoutGrid size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8 pt-5">
+        {categories.map((category) => (
+          <CatagoryItem category={category} key={category.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CatalogPage;
