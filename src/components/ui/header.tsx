b79f7ba68ca740feded6292518930655345c5341
Pedro dos Santos <pedroblues281@gmail.com>
2025-03-04 11:22:23 -0300

    feat: ensure user goes to home page on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -88,11 +88,17 @@ const Header = () => {
                 Fazer Logout
               </Button>
             )}
-
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <HomeIcon size={"16"} />
-              Inicio
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={"16"} />
+                  Inicio
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant={"outline"} className="w-full justify-start gap-2">
               <PercentIcon size={"16"} />
@@ -113,9 +119,11 @@ const Header = () => {
           </div>
         </SheetContent>
       </Sheet>
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">PS</span> Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">PS</span> Store
+        </h1>
+      </Link>
       <Button size={"icon"} variant={"outline"}>
         <ShoppingCartIcon />
       </Button>
