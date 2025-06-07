8eb5e8635e39b8686ded79135569faeeb3f2854b
Pedro dos Santos <pedroblues281@gmail.com>
2025-06-07 17:32:13 -0300

    feat: add HOST_URL

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -11,8 +11,8 @@ export const crateCheckcout = async (prodycts: CartProduct[]) => {
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: 'http://localhost:3000',
-    cancel_url: 'http://localhost:3000',
+    success_url: process.env.HOST_URL,
+    cancel_url: process.env.HOST_URL,
     line_items: prodycts.map((product) => {
       return {
         price_data: {
